1e0b4e719a371a0991ff326df9244091b7e7196a
jonathanpochini <44929776+jonathanpochini@users.noreply.github.com>
2022-04-08 23:33:05 -0500

    adding ffiles

--- app.py
+++ app.py
@@ -7,3 +7,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+
+@app.route('/binance')
+def binance():
+    return render_template('binance.html', title="Binance")
